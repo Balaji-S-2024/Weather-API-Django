d6301498190b0499a75f3fa44d993e2fd824f5e8
Balaji <113788978+Balaji-S-2024@users.noreply.github.com>
2023-08-17 20:30:21 +0530

    Update settings.py for deploy

--- weatherproject/settings.py
+++ weatherproject/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-8qa_hoek5km$g%9x(=@kz*!uk4rwqs(=6)-o27db__xm#5%t^q'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
